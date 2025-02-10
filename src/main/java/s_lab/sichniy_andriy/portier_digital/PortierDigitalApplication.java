5c6e838962021c2544bd1877a7cc5734e42d4859
Andriy Sichniy <sichniy.andriy@gmail.com>
2025-02-10 21:13:34 +0200

    initialize project from boot and started frontend

---
+++ src/main/java/s_lab/sichniy_andriy/portier_digital/PortierDigitalApplication.java
@@ -0,0 +1,13 @@
+package s_lab.sichniy_andriy.portier_digital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortierDigitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortierDigitalApplication.class, args);
+    }
+
+}
