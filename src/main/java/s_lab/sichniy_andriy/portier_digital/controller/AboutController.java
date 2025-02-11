ab68d2e10f12dd5b598277ce2e3444707cf97d8e
Andriy Sichniy <sichniy.andriy@gmail.com>
2025-02-11 15:58:27 +0200

    add service impl

--- src/main/java/s_lab/sichniy_andriy/portier_digital/controller/AboutController.java
+++ src/main/java/s_lab/sichniy_andriy/portier_digital/controller/AboutController.java
@@ -1,12 +1,22 @@
 package s_lab.sichniy_andriy.portier_digital.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import s_lab.sichniy_andriy.portier_digital.service.AboutService;
 
 @Controller
 @RequestMapping( "/about" )
 public class AboutController {
+
+    private final AboutService aboutService;
+
+    public AboutController(
+            @Autowired AboutService aboutService
+    ) {
+        this.aboutService = aboutService;
+    }
 
     @GetMapping( { "", "/" } )
     public String about() {
--- src/main/java/s_lab/sichniy_andriy/portier_digital/service/impl/AboutServiceImpl.java
+++ src/main/java/s_lab/sichniy_andriy/portier_digital/service/impl/AboutServiceImpl.java
@@ -1,7 +1,28 @@
 package s_lab.sichniy_andriy.portier_digital.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import s_lab.sichniy_andriy.portier_digital.model.mapper.CompaniesMapper;
+import s_lab.sichniy_andriy.portier_digital.model.mapper.ContactsMapper;
+import s_lab.sichniy_andriy.portier_digital.model.mapper.SkillMapper;
 import s_lab.sichniy_andriy.portier_digital.service.AboutService;
 
+
+@Service
 public class AboutServiceImpl implements AboutService {
+
+    private final CompaniesMapper companiesMapper;
+    private final ContactsMapper contactsMapper;
+    private final SkillMapper skillMapper;
+
+    public AboutServiceImpl(
+            @Autowired CompaniesMapper companiesMapper,
+            @Autowired ContactsMapper contactsMapper,
+            @Autowired SkillMapper skillMapper
+    ) {
+        this.companiesMapper = companiesMapper;
+        this.contactsMapper = contactsMapper;
+        this.skillMapper = skillMapper;
+    }
 
 }
