16272864d1d508553fd8e939f0fd2725ac36be0f
Andriy Sichniy <sichniy.andriy@gmail.com>
2025-02-14 14:02:25 +0200

    refactoring in AboutController

--- src/main/java/s_lab/sichniy_andriy/portier_digital/controller/AboutController.java
+++ src/main/java/s_lab/sichniy_andriy/portier_digital/controller/AboutController.java
@@ -3,9 +3,9 @@ package s_lab.sichniy_andriy.portier_digital.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import s_lab.sichniy_andriy.portier_digital.model.dto.CompanyDto;
 import s_lab.sichniy_andriy.portier_digital.model.dto.ContactDto;
 import s_lab.sichniy_andriy.portier_digital.model.dto.SkillDto;
@@ -25,14 +25,17 @@ public class AboutController {
 
 
     @GetMapping( { "", "/" } )
-    public String about(Model model) {
+    public ModelAndView about(ModelAndView modelAndView) {
         List<CompanyDto> companyDtoList = aboutService.getAllCompanies();
         List<ContactDto> contactDtoList = aboutService.getAllContacts();
         List<SkillDto> skillDtoList = aboutService.getAllSkills();
-        model.addAttribute("companies", companyDtoList);
-        model.addAttribute("contacts", contactDtoList);
-        model.addAttribute("skills", skillDtoList);
-        return "about";
+
+        modelAndView.addObject("companies", companyDtoList);
+        modelAndView.addObject("contacts", contactDtoList);
+        modelAndView.addObject("skills", skillDtoList);
+        modelAndView.setViewName("about");
+
+        return modelAndView;
     }
 
 }
