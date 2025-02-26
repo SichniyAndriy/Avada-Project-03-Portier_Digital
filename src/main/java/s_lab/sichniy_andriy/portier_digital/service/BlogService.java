4607d71b45512bf59ce4fe0c0268e8160e4d1722
Andriy Sichniy <sichniy.andriy@gmail.com>
2025-02-26 18:43:49 +0200

    add methods to a blog service

--- src/main/java/s_lab/sichniy_andriy/portier_digital/service/BlogService.java
+++ src/main/java/s_lab/sichniy_andriy/portier_digital/service/BlogService.java
@@ -1,6 +1,7 @@
 package s_lab.sichniy_andriy.portier_digital.service;
 
 import java.util.List;
+import org.springframework.data.domain.Page;
 import s_lab.sichniy_andriy.portier_digital.model.dto.ArticleDto;
 import s_lab.sichniy_andriy.portier_digital.model.dto.SubscriberDto;
 
@@ -8,6 +9,14 @@ import s_lab.sichniy_andriy.portier_digital.model.dto.SubscriberDto;
 public interface BlogService {
 
     List<ArticleDto> getAllArticles();
+
+    Page<ArticleDto> getArticlePage(int page, int size, String col);
+
+    ArticleDto getArticleById(long id);
+
+    boolean deleteById(long id);
+
+    long saveArticle(ArticleDto articleDto);
 
     List<SubscriberDto> getAllSubscribers();
 
--- src/main/java/s_lab/sichniy_andriy/portier_digital/service/impl/BlogServiceImpl.java
+++ src/main/java/s_lab/sichniy_andriy/portier_digital/service/impl/BlogServiceImpl.java
@@ -1,10 +1,15 @@
 package s_lab.sichniy_andriy.portier_digital.service.impl;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 import s_lab.sichniy_andriy.portier_digital.model.Article;
 import s_lab.sichniy_andriy.portier_digital.model.Subscriber;
 import s_lab.sichniy_andriy.portier_digital.model.dto.ArticleDto;
@@ -37,14 +42,51 @@ public class BlogServiceImpl implements BlogService {
         this.subscriberMapper = subscriberMapper;
     }
 
-
+ //                                 ARTICLES                                 \\
+// ===========================================================================\\
     @Override
     public List<ArticleDto> getAllArticles() {
-        List<Article> articles =
-                articlesRepository.findAll(Sort.by(Direction.DESC, "id"));
+        List<Article> articles = articlesRepository.findAll(Sort.by(Direction.DESC, "id"));
         return articleMapper.toDto(articles);
     }
 
+    @Override
+    public Page<ArticleDto> getArticlePage(int page, int size, String col) {
+        long amountAll = articlesRepository.count();
+        long pages = amountAll / size + (amountAll % size == 0 ? 0 : 1);
+        if (page > pages) {
+            page = (int) pages;
+        }
+        Page<Article> articlePage =
+                articlesRepository.findAll(PageRequest.of(--page, size, Sort.by(col)));
+        return articlePage.map(articleMapper::toDto);
+    }
+
+    @Override
+    public ArticleDto getArticleById(long id) {
+        Optional<Article> articleOptional = articlesRepository.findById(id);
+        return articleOptional.map(articleMapper::toDto).orElse(null);
+    }
+
+    @Override
+    public long saveArticle(ArticleDto articleDto) {
+        Article article = articleMapper.toEntity(articleDto);
+        Article saved = articlesRepository.save(article);
+        return saved.getId();
+    }
+
+    @Override
+    @Transactional(isolation = Isolation.SERIALIZABLE)
+    public boolean deleteById(long id) {
+        boolean res = articlesRepository.existsById(id);
+        if (res) {
+            articlesRepository.deleteById(id);
+        }
+        return res;
+    }
+
+// ===========================================================================\\
+//                                  COMPANIES                                  \\
     @Override
     public List<SubscriberDto> getAllSubscribers() {
         List<Subscriber> subscribers = subscribersRepository.findAll(Sort.by(Direction.ASC, "email"));
