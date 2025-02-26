f633ceae92120324be0956656795839e223a6e83
Andriy Sichniy <sichniy.andriy@gmail.com>
2025-02-26 18:43:59 +0200

    add ArticlesRepository

--- src/main/java/s_lab/sichniy_andriy/portier_digital/repository/ArticlesRepository.java
+++ src/main/java/s_lab/sichniy_andriy/portier_digital/repository/ArticlesRepository.java
@@ -1,6 +1,9 @@
 package s_lab.sichniy_andriy.portier_digital.repository;
 
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -10,5 +13,18 @@ public interface ArticlesRepository extends JpaRepository<Article, Long>, JpaSpe
 
     @Override
     List<Article> findAll(Sort sort);
+
+    Page<Article> findAll(Pageable pageable);
+
+    long count();
+
+    List<Article> findAllBy(PageRequest pageRequest);
+
+    boolean existsById(Long id);
+
+    void deleteById(Long id);
+
+    @Override
+    <S extends Article> S save(S entity);
 
 }
